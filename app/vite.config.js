b7e8510c335522c614a84729ece887c2e3f155fb
Alfian Danny Armanta <alfiandannyarmanta@gmail.com>
2025-02-06 11:28:01 +0700

    feat: setting vite config
    Author: @arufiand

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    plugins: [laravel(["resources/js/app.jsx"])],
+    resolve: {
+        alias: {
+            "@": "/resources/js"
+        }
+    },
     server: {
-        host: '0.0.0.0',  // Bind to all interfaces
-        port: 5173,        // Keep default Vite port
-        strictPort: true,  // Ensure it doesn't switch ports
+        host: "0.0.0.0", // Bind to all interfaces
+        port: 5173, // Keep default Vite port
+        strictPort: true, // Ensure it doesn't switch ports
         hmr: {
-            host: 'localhost', // Change this to WSL2 IP if needed
+            host: "localhost" // Change this to WSL2 IP if needed
         }
     }
 });
